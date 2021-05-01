1ee5cea60b00cca28801b1dfefc2aedda083cd63
Devashray <2704devashray@gmail.com>
2021-05-01 14:15:15 +0530

    added go back functionality

--- src/Components/NoData.js
+++ src/Components/NoData.js
@@ -4,15 +4,13 @@ import GetInput from "./GetInput";
 const NoData = () => {
     console.log("NODATA");
 
-const handleClick = () => {
-    return <GetInput/>
-}
+
 
 
     return (
     <>
         <h3>No Items Found</h3>
-        <button onClick = {handleClick}>Go Back</button>
+        <button><a href = "./GetData.js">Go Back</a></button>
     </>
     )
 }
